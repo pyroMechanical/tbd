84f37fca4913393993b734c1ecd33de4e7d48c39
pyromechanical <andreaogara@gmail.com>
2024-01-17 22:34:26 -0500

    added skeleton of basic ui overlay

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use enigo::{self, KeyboardControllable, Key};
+use enigo::{self, Key, KeyboardControllable};
 use gilrs::{Axis, Button, Event, Gilrs};
+
+use druid::{Widget, WindowDesc, Color, RenderContext, WidgetExt};
 
 use std::io::Write;
 
@@ -48,15 +50,16 @@ fn transform_input_vector(raw: Vector2) -> Vector2 {
     normalized
 }
 
-static CHARACTERS: [[Option<char>; 4]; 8] = [
-    [Some('a'), Some('b'), Some('c'), None],
-    [Some('d'), Some('e'), Some('f'), None],
-    [Some('g'), Some('h'), Some('i'), None],
-    [Some('j'), Some('k'), Some('l'), None],
-    [Some('m'), Some('n'), Some('o'), None],
-    [Some('p'), Some('q'), Some('r'), Some('s')],
-    [Some('t'), Some('u'), Some('v'), None],
-    [Some('w'), Some('x'), Some('y'), Some('z')],
+static CHARACTERS: [[Option<char>; 8]; 9] = [
+    [None, None, None, None, None, None, None, None],
+    [None, Some('a'), None, Some('b'), None, Some('c'), None, None],
+    [None, Some('d'), None, Some('e'), None, Some('f'), None, None],
+    [None, Some('g'), None, Some('h'), None, Some('i'), None, None],
+    [None, Some('j'), None, Some('k'), None, Some('l'), None, None],
+    [None, Some('m'), None, Some('n'), None, Some('o'), None, None],
+    [None, Some('p'), None, Some('q'), None, Some('r'), None, Some('s')],
+    [None, Some('t'), None, Some('u'), None, Some('v'), None, None],
+    [None, Some('w'), None, None, None, Some('y'), None, Some('z')],
 ];
 
 static TYPE_MAGNITUDE_THRESHOLD: f32 = 0.7f32;
@@ -65,7 +68,70 @@ static TYPE_RETURN_THRESHOLD: f32 = 0.5f32;
 
 use std::f32::consts::PI;
 
+fn make_widget() -> impl Widget<()> {
+    use druid::widget::*;
+
+    let char_label = |maybe_char: Option<_>| {
+        let text = maybe_char.map_or(' ', |x| x).to_string();
+        AspectRatioBox::new(Flex::column().main_axis_alignment(MainAxisAlignment::Center).with_flex_child(Label::<()>::new(text).with_text_alignment(druid::text::TextAlignment::Center), 1.0), 1.0)
+    };
+
+    let character_wheel = |chars: &[Option<_>;8]| {
+        Flex::column()
+        .with_flex_child(Flex::row()
+        .with_flex_child(char_label(chars[0]), 1.0)
+        .with_flex_child(char_label(chars[1]), 1.0)
+        .with_flex_child(char_label(chars[2]), 1.0)
+        .must_fill_main_axis(true), 1.0)
+        .with_flex_child(Flex::row()
+        .with_flex_child(char_label(chars[7]), 1.0)
+        .with_flex_child(char_label(None), 1.0)
+        .with_flex_child(char_label(chars[3]), 1.0)
+        .must_fill_main_axis(true), 1.0)
+        .with_flex_child(Flex::row()
+        .with_flex_child(char_label(chars[6]), 1.0)
+        .with_flex_child(char_label(chars[5]), 1.0)
+        .with_flex_child(char_label(chars[4]), 1.0)
+        .must_fill_main_axis(true), 1.0)
+        .must_fill_main_axis(true)
+    };
+
+    let box_factory = |char_array: &[Option<_>;8]| {
+        druid::widget::SizedBox::new(AspectRatioBox::new(Container::new(character_wheel(char_array)).background(Color::rgba8(127, 127, 255, 127)) , 1.0f64)).expand()
+    };
+
+    let row_factory = |char_arrays: &[[Option<_>; 8]; 9], index1, index2, index3| {
+        druid::widget::Flex::row()
+        .must_fill_main_axis(true)
+        .with_flex_child(box_factory(&char_arrays[index1]), 1.0)
+        .with_default_spacer()
+        .with_flex_child(box_factory(&char_arrays[index2]), 1.0)
+        .with_default_spacer()
+        .with_flex_child(box_factory(&char_arrays[index3]), 1.0)
+    };
+
+    druid::widget::Flex::column()
+    .must_fill_main_axis(true)
+    .with_flex_child(row_factory(&CHARACTERS, 1, 2, 3), 1.0)
+    .with_default_spacer()
+    .with_flex_child(row_factory(&CHARACTERS, 8, 0, 4), 1.0)
+    .with_default_spacer()
+    .with_flex_child(row_factory(&CHARACTERS, 7, 6, 5), 1.0)
+}
+
 fn main() {
+    let main_window = WindowDesc::new(
+        make_widget()
+    )
+    .set_always_on_top(true)
+    .window_size((200.0, 200.0))
+    .set_position((50.0f64, 50.0f64))
+    .transparent(true)
+    .show_titlebar(false);
+
+    druid::AppLauncher::with_window(main_window)
+        .launch(())
+        .expect("Failed to launch application.");
     let mut can_type = true;
     let mut can_toggle_caps = true;
     let mut gilrs = Gilrs::new().unwrap();
@@ -92,22 +158,19 @@ fn main() {
 
                 if gamepad.is_pressed(Button::East) {
                     enigo.key_down(Key::Backspace);
-                }
-                else {
+                } else {
                     enigo.key_up(Key::Backspace);
                 }
 
                 if gamepad.is_pressed(Button::LeftThumb) {
                     enigo.key_down(Key::Shift);
-                }
-                else {
+                } else {
                     enigo.key_up(Key::Shift);
                 }
                 if gamepad.is_pressed(Button::RightTrigger) && can_toggle_caps {
                     enigo.key_click(Key::CapsLock);
                     can_toggle_caps = false;
-                }
-                else if !gamepad.is_pressed(Button::RightTrigger) && !can_toggle_caps {
+                } else if !gamepad.is_pressed(Button::RightTrigger) && !can_toggle_caps {
                     can_toggle_caps = true;
                 }
 
@@ -141,12 +204,12 @@ fn main() {
                     continue;
                 }
 
-                let left_turn = left.angle()/(PI*2.0f32);
+                let left_turn = left.angle() / (PI * 2.0f32);
                 let left_angle: u8 = ((left_turn * 8.0f32) + 0.5f32) as u8 % 8;
 
                 let chars = CHARACTERS[left_angle as usize];
 
-                let right_turn = right.angle()/(PI*2.0f32);
+                let right_turn = right.angle() / (PI * 2.0f32);
                 let right_angle: u8 = ((right_turn * 4.0f32) + 0.5f32) as u8 % 4;
                 //println!("{}", ((right_turn * 4.0f32) + 0.5f32) % 4.0f32);
 
@@ -154,13 +217,12 @@ fn main() {
 
                 if right.magnitude() >= TYPE_MAGNITUDE_THRESHOLD {
                     if can_type {
-                        if let Some(c) = chars[right_angle as usize]{
+                        if let Some(c) = chars[right_angle as usize] {
                             enigo.key_click(Key::Layout(c));
                             can_type = false;
                         }
                     }
-                }
-                else if right.magnitude() <= TYPE_RETURN_THRESHOLD {
+                } else if right.magnitude() <= TYPE_RETURN_THRESHOLD {
                     can_type = true;
                 }
             }
